a107390d458c4804bb0ff5f427f477ee8d8405ea
pravee20029 <senanayakapraveen@gmail.com>
2026-04-28 23:35:49 +0530

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,9 @@
+
+(function () {
+  var s = document.createElement("script");
+  s.dataset.zone = "10358473";
+  s.src = "https://al5sm.com/tag.min.js";
+  s.async = true;
+
+  document.head.appendChild(s);
+})();
